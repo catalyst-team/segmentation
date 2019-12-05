de9d4dd411c7bfeeb3e506e2256a131f7a693a52
Yauheni Kachan <imcodelike@gmail.com>
2019-12-05 17:50:39 +0300

    update requirements (#11)

--- src/transforms.py
+++ src/transforms.py
@@ -26,27 +26,9 @@ class ToTensor(A.core.transforms_interface.DualTransform):
         return kwargs
 
 
-class ImageToRGB(A.core.transforms_interface.ImageOnlyTransform):
-    """Convert the input image to RGB if it grayscale"""
-
-    def __init__(self, always_apply: bool = False, p: float = 1.0):
-        """
-        Args:
-            p (float): probability of applying the transform
-        """
-        super().__init__(always_apply=always_apply, p=p)
-
-    def apply(self, img: np.ndarray, **params) -> np.ndarray:
-        """Convert the input image to RGB if it grayscale"""
-        if len(img.shape) < 3 or img.shape[-1] == 1:
-            img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-        return img
-
-
 def pre_transforms(image_size: int = 256):
     """Transforms that always be applied before other transformations"""
     transforms = Compose([
-        ImageToRGB(),
         LongestMaxSize(max_size=image_size),
         PadIfNeeded(
             image_size, image_size, border_mode=cv2.BORDER_CONSTANT
