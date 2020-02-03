628494906980661a06cbc239acc3a5f1ccb47e5d
Yauheni Kachan <19803638+bagxi@users.noreply.github.com>
2020-02-03 11:06:58 +0300

    upd scripts

--- bin/_check_codestyle.sh
+++ bin/_check_codestyle.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+set -e
 
 # Parse -s flag which tells us that we should skip inplace yapf
+echo 'parse -s flag'
 skip_inplace=""
 while getopts ":s" flag; do
   case "${flag}" in
@@ -8,21 +10,29 @@ while getopts ":s" flag; do
   esac
 done
 
+echo "isort -rc --check-only --settings-path ./setup.cfg"
+isort -rc --check-only --settings-path ./setup.cfg
 
 # stop the build if there are any unexpected flake8 issues
+echo './bin/flake8.sh'
 bash ./bin/flake8.sh --count \
     --config=./setup.cfg \
     --show-source --statistics
 
 # exit-zero treats all errors as warnings.
+echo '~ ~ ~ ~ ~ ~ ~ flake8 warnings ~ ~ ~ ~ ~ ~ ~' 1>&2
 flake8 . --count --exit-zero \
     --max-complexity=10 \
     --config=./setup.cfg \
     --statistics
+echo '~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~' 1>&2
 
 # test to make sure the code is yapf compliant
+echo './bin/yapf.sh'
 if [[ -f ${skip_inplace} ]]; then
+    echo 'yapf.sh --all'
     bash ./bin/yapf.sh --all
 else
+    echo 'yapf.sh --all-in-place'
     bash ./bin/yapf.sh --all-in-place
 fi
--- teamcity/tests.sh
+++ teamcity/tests.sh
@@ -4,8 +4,5 @@ pip install -r requirements/requirements.txt
 echo "pip install -r requirements/requirements-dev.txt"
 pip install -r requirements/requirements-dev.txt
 
-echo "isort -rc --check-only --settings-path ./setup.cfg"
-isort -rc --check-only --settings-path ./setup.cfg
-
 echo "make check-style"
 make check-style
